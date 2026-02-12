3152e03bfc2e6e9b020f5a0d625520b157564bee
mareksuchodolski12-hash <mareksuchodolski12@gmail.com>
2026-02-12 01:19:12 +0100

    Add Google Analytics integration component

---
+++ app/analytics.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Script from "next/script"
+
+export default function Analytics() {
+  const GA_ID = "G-95496XJE7V";
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
